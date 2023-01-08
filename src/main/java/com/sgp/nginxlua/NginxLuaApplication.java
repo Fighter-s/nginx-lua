f296e328003a47be062c27376cb1a41963351eef
Fighter-s <2274378647@qq.com>
2023-01-08 14:51:06 +0800

    上传笔记

---
+++ src/main/java/com/sgp/nginxlua/NginxLuaApplication.java
@@ -0,0 +1,13 @@
+package com.sgp.nginxlua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NginxLuaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NginxLuaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sgp/nginxlua/NginxLuaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgp.nginxlua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NginxLuaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
